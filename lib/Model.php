a157a6253b62abc1ba1883627bea6700e315a903
koh-jjj-gt <docomo.gg.712@gmail.com>
2020-02-06 13:31:27 +0900

    mysql 5

--- lib/Model.php
+++ lib/Model.php
@@ -20,8 +20,9 @@ class Model {
       $this->db['dbname'] = ltrim($this->db['path'], '/');
       $this->dsn = "mysql:host={$this->db['host']};dbname={$this->db['dbname']};charset=utf8";
       $this->db = new \PDO($this->dsn, $this->db['user'], $this->db['pass']);
-      $this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+//    $this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 //    $this->db->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 //    $this->db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
     } catch (\PDOException $e) {
       echo $e->getMessage();
