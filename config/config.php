a59bd0cb8e12c7bd6eb19b505cfc8a826d3a381d
koh-jjj-gt <docomo.gg.712@gmail.com>
2020-02-06 12:41:51 +0900

    mysql 5

--- config/config.php
+++ config/config.php
@@ -7,15 +7,6 @@ define('DB_USERNAME', 'bb06f54de49788');
 define('DB_PASSWORD', 'a5407c5b');
 
 
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-$db['dbname'] = ltrim($db['path'], '/');
-$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-$db = new PDO($dsn, $db['user'], $db['pass']);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
 require_once(__DIR__ . '/../lib/functions.php');
--- lib/Model.php
+++ lib/Model.php
@@ -4,10 +4,26 @@ namespace MyApp;
 
 class Model {
   protected $db;
+  protected $dsn;
 
+//   public function __construct() {
+//     try {
+//       $this->db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+//     } catch (\PDOException $e) {
+//       echo $e->getMessage();
+//       exit;
+//     }
+//   }
   public function __construct() {
     try {
-      $this->db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+        $this->db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+        $this->db['dbname'] = ltrim($db['path'], '/');
+        $this->dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+        $this->db = new \PDO($dsn, $db['user'], $db['pass']);
+        $this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        $this->db->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+//       $this->db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
     } catch (\PDOException $e) {
       echo $e->getMessage();
       exit;
