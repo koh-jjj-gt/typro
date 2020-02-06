06f08fedf04aad55530ee09e97eeec6f569bb3b7
koh-jjj-gt <docomo.gg.712@gmail.com>
2020-02-06 13:26:46 +0900

    mysql 5

--- public_html/logout.php
+++ public_html/logout.php
@@ -4,10 +4,10 @@ require_once(__DIR__ . '/../config/config.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  if (!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
-    echo "無効なトークンです！";
-    exit;
-  }
+//   if (!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
+//     echo "無効なトークンです！";
+//     exit;
+//   }
 
   $_SESSION = [];
 
