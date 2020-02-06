e5eb2ff1e7f235df49a7c3bd6a047dee6c7db25c
koh-jjj-gt <docomo.gg.712@gmail.com>
2020-02-06 12:33:48 +0900

    mysql 4

--- config/config.php
+++ config/config.php
@@ -2,9 +2,19 @@
 
 ini_set('display_errors', 1);
 
-define('DSN', 'mysql:dbhost=localhost;dbname=typrogame_php');
-define('DB_USERNAME', 'dbuser');
-define('DB_PASSWORD', 'jfa683BjI');
+define('DSN', 'mysql:dbhost=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_cdfd56027dcc86e');
+define('DB_USERNAME', 'bb06f54de49788');
+define('DB_PASSWORD', 'a5407c5b');
+
+
+// $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+// $db['dbname'] = ltrim($db['path'], '/');
+// $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+// $db = new PDO($dsn, $db['user'], $db['pass']);
+// $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
